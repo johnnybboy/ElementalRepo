069e06f306bb40e532870a4d8e3aea0d3d2ab445
johnnybboy <brantonj2795@my.uwstout.edu>
2021-10-11 11:05:36 -0500

    Added to UI Script

--- ElementalProject/Assets/Scripts/UIManager.cs
+++ ElementalProject/Assets/Scripts/UIManager.cs
@@ -11,6 +11,7 @@ public class UIManager : MonoBehaviour
     public Text healthText;
     public Text enemyCountText;
     public Text endText;
+    public Image heartImage;
 
     // Start is called before the first frame update
     void Start()
