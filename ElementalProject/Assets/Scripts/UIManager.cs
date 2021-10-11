f46dee8aee8492bd4cc0f75801455466087eb40d
johnnybboy <brantonj2795@my.uwstout.edu>
2021-10-11 17:08:32 -0500

    Fixed an issue with UI

--- ElementalProject/Assets/Scripts/UIManager.cs
+++ ElementalProject/Assets/Scripts/UIManager.cs
@@ -11,16 +11,10 @@ public class UIManager : MonoBehaviour
     public Text healthText;
     public Text enemyCountText;
     public Text endText;
-    public Image mainHeart;
-    public int numberOfHearts = 0;
-    private Image[] hearts;
-
-    public float xOffset = 32f;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlaceHearts(5);
         player = GameObject.Find("Player").GetComponent<PlayerController>();
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
@@ -44,20 +38,6 @@ public class UIManager : MonoBehaviour
         else
         {
             endText.enabled = false;
-        }
-    }
-
-    void PlaceHearts(int num)
-    {
-        numberOfHearts = 0;
-        hearts = new Image[num];
-        float spacer = mainHeart.transform.position.x;
-        for (int i = 0; i < num; i++)
-        {
-            hearts[i] = Instantiate(mainHeart, mainHeart.transform.position, mainHeart.transform.rotation);
-            hearts[i].transform.position = new Vector2(spacer, mainHeart.transform.position.y);
-            spacer += xOffset;
-            numberOfHearts++;
         }
     }
 }
