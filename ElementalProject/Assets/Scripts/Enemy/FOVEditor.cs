97e445d69483859fc26713f5c7acad90a0ebc7b0
Nate L <92316471+nateclaw@users.noreply.github.com>
2021-10-13 10:27:39 -0500

    Adds FOV,FOVEditor,Movement

---
+++ ElementalProject/Assets/Scripts/Enemy/FOVEditor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor (typeof(FieldOfView))]
+public class FOVEditor :  Editor
+{
+   /* void OnSceneGUI()
+    {
+        FieldOfView fov = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position,Vector2.up,Vector2.forwards)
+    }
+   */
+}
