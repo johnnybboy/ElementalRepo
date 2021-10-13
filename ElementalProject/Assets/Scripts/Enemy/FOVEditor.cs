ec7797285da986a11227152ecdbe8942805dcde9
Nate L <92316471+nateclaw@users.noreply.github.com>
2021-10-13 11:11:18 -0500

    edited code

--- ElementalProject/Assets/Scripts/Enemy/FOVEditor.cs
+++ ElementalProject/Assets/Scripts/Enemy/FOVEditor.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-[CustomEditor (typeof(FieldOfView))]
+/*[CustomEditor (typeof(FieldOfView))]
 public class FOVEditor :  Editor
 {
-   /* void OnSceneGUI()
+    void OnSceneGUI()
     {
         FieldOfView fov = (FieldOfView)target;
         Handles.color = Color.white;
         Handles.DrawWireArc(fov.transform.position,Vector2.up,Vector2.forwards)
     }
-   */
+   
 }
+    */
