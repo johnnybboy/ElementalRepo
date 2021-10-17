5cf339d0ecc828855fedee6999769d16cb1d9ca6
nateclaw <92316471+nateclaw@users.noreply.github.com>
2021-10-17 15:02:53 -0500

    Added Movement options for enemy

--- ElementalProject/Assets/Scripts/Enemy/Movement.cs
+++ ElementalProject/Assets/Scripts/Enemy/Movement.cs
@@ -2,27 +2,98 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Movement : MonoBehaviour
 {
-    Rigidbody2D rd;
-    public float moveSpeed = 6;
-    
+    Rigidbody2D rb;
+    public int WanderSpeed = 3;
+    public int PatrolSpeed = 3;
+    public enum Movement_Type { idle, patrol, sleep, attack,wander }
+    public Movement_Type moveType;
     Vector2 vel;
-
+    Vector2 startPos;
+    Vector2 newPos;
+    public float patrolLBounds;
+    public float partolRBounds;
+    public bool move_Right = true;
+    
     // Start is called before the first frame update
     void Start()
     {
-        rd = GetComponent<Rigidbody2D>();    
+        if(WanderSpeed <= 0 || PatrolSpeed <= 0)
+        {
+            WanderSpeed = 1;
+            PatrolSpeed = 1;
+        }
+        rb = GetComponent<Rigidbody2D>();
+        startPos = new Vector2(rb.position.x,rb.position.y);
+        DirectionChange();
+        //print(startPos); //to see if startPos is working
     }
+    void DirectionChange()
+    {
+        int x = 0;
+        int y = 0;
+        while (x > -(WanderSpeed) / 2 && x < (WanderSpeed) / 2)
+        {
+            x = Random.Range(-WanderSpeed, WanderSpeed);
+        }
+        while (y > -1 && y < 1)
+        {
+            y = Random.Range(-WanderSpeed, WanderSpeed);
+        }
 
+        newPos = new Vector2(-x, y);
+    }
     // Update is called once per frame
     void Update()
     {
-        vel = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Virtical")).normalized * moveSpeed;
+        
+        if(moveType == Movement_Type.patrol)
+        {
+            if(move_Right == true) 
+            {
+                MarchRight();
+            }
+            else
+            {
+                MarchLeft();
+            }
+            
+        }
+        else if(moveType == Movement_Type.wander)
+        {
+            rb.AddForce(newPos);
+        }
     }
 
-    private void FixedUpdate()
+    
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        rd.MovePosition(rd.position + vel * Time.fixedDeltaTime);
+        DirectionChange();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        DirectionChange();
+    }
+
+    private void MarchRight()
+    {
+        rb.AddForce(new Vector2(PatrolSpeed, 0));
+        if (rb.position.x >= startPos.x + partolRBounds)
+        {
+            move_Right = false;
+        }
+        
+    }
+    private void MarchLeft()
+    {
+        rb.AddForce(new Vector2(-PatrolSpeed, 0));
+        if (rb.position.x <= startPos.x - patrolLBounds)
+        {
+            move_Right = true;
+        }
+
     }
 }
