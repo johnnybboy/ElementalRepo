8c96b4c638aa97ea15312fe38055395426709abd
nateclaw <92316471+nateclaw@users.noreply.github.com>
2021-10-17 22:33:46 -0500

    pushed but not done

---
+++ ElementalProject/Assets/Scripts/Enemy/Projectile.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public GameObject player;
+
+    public enum Projectile_Type { curve, straight, bounce, boomerang}
+    public Projectile_Type projectileType;
+
+    Vector2 startPos; //starting position
+
+    public bool move_Right = true; // starts patrol in the right direction
+    public bool move_Up = true;    // starts patrol in the up direction
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        startPos = new Vector2(rb.position.x, rb.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool direction = playerDirection();
+        if (projectileType == Projectile_Type.straight)
+        {
+
+            rb.AddForce(new Vector2(1, 0));
+        }
+    }
+
+    private bool playerDirection()
+    {
+        if (player.transform.position.x < rb.position.x) // fires left
+        {
+            return false;
+        }
+        else if (player.transform.position.x > rb.position.x) //fires right
+        {
+            return true;
+        }
+        else { return false; }
+    }
+}
