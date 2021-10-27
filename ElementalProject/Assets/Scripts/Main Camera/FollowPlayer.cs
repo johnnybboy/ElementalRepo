433e04e14d291f3fc55b4d01be700a6e0e6338d2
johnnybboy <brantonj2795@my.uwstout.edu>
2021-10-27 09:21:00 -0500

    Added Test_scene

--- ElementalProject/Assets/Scripts/Main Camera/FollowPlayer.cs
+++ ElementalProject/Assets/Scripts/Main Camera/FollowPlayer.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
-{
-    public Transform player;
+{   
+    //finds the game object with the Player tag and follows its transform
+
+    private Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
