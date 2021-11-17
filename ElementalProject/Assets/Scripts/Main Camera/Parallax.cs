85ad29e303380160f23204bdd51bc649803437e6
Devlyn7299 <nwokeukud2011@my.uwstout.edu>
2021-11-17 10:03:53 -0600

    Current Parallax Effect
    Doesn't work that well in the y direction

---
+++ ElementalProject/Assets/Scripts/Main Camera/Parallax.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, lengthY, startPos, startPosY;
+    public GameObject cam;
+    public float parallexEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        //length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        //startPosY = transform.position.y;
+        //lengthY = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    void FixedUpdate()
+    {
+        //float temp = (cam.transform.position.x * (1 - parallexEffect));
+        float dist = (cam.transform.position.x * parallexEffect);
+
+        //float tempY = (cam.transform.position.y * (1 - parallexEffect));
+        //float distY = (cam.transform.position.y * parallexEffect / 2);
+
+        transform.position = new Vector3(startPos - dist, transform.position.y, transform.position.z);
+
+        //transform.position = new Vector3(startPos - dist, startPosY - distY, transform.position.z);
+
+        //if (temp > startPos + length) startPos += length;
+        //else if (temp < startPos - length) startPos -= length;
+    }
+
+}
