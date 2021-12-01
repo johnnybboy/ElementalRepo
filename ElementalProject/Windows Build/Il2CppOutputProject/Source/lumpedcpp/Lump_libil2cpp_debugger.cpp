b1c2abc6b4b6d908ad2b07789919300a45aa1c09
johnnybboy <brantonj2795@my.uwstout.edu>
2021-12-01 08:27:52 -0600

    FixAttempt1

--- ElementalProject/Windows Build/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\leimern1978\Documents\GitHub\ElementalRepo\ElementalProject\Windows Build\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
--- ElementalProject/Windows Build/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_vm-utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\leimern1978\Documents\GitHub\ElementalRepo\ElementalProject\Windows Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\BlobReader.cpp"
-#include "C:\Users\leimern1978\Documents\GitHub\ElementalRepo\ElementalProject\Windows Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\Debugger.cpp"
-#include "C:\Users\leimern1978\Documents\GitHub\ElementalRepo\ElementalProject\Windows Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeDelegateMethodCache.cpp"
-#include "C:\Users\leimern1978\Documents\GitHub\ElementalRepo\ElementalProject\Windows Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\NativeSymbol.cpp"
-#include "C:\Users\leimern1978\Documents\GitHub\ElementalRepo\ElementalProject\Windows Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\VmStringUtils.cpp"
-#include "C:\Users\leimern1978\Documents\GitHub\ElementalRepo\ElementalProject\Windows Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System.Threading\Interlocked.cpp"
-#include "C:\Users\leimern1978\Documents\GitHub\ElementalRepo\ElementalProject\Windows Build\Il2CppOutputProject\IL2CPP\libil2cpp\vm-utils\icalls\mscorlib\System\Math.cpp"
